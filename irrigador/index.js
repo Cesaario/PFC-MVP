bce9acf8340d33863a9334c67874ffe1394b6a27
Gustavo <gs.cesario1998@gmail.coim>
2021-11-14 17:27:19 -0300

    irrigador

---
+++ irrigador/index.js
@@ -0,0 +1,48 @@
+let umidade = 50;
+let irrigador = false;
+
+function rotinaJardim() {
+  let velocidadeBase = -1;
+
+  if (irrigador)
+    velocidadeBase = 3;
+
+  let novaUmidade = umidade + velocidadeBase;
+  umidade = Math.max(0, Math.min(100, novaUmidade))
+}
+
+function rotinaIrrigador() {
+  const limiarAlto = document.querySelector('input[name="LA"]:checked').value;
+  const limiarBaixo = document.querySelector('input[name="LB"]:checked').value;
+
+  const valorLimiarAlto = Number(limiarAlto.replace("%", ""))
+  const valorLimiarBaixo = Number(limiarBaixo.replace("%", ""))
+
+  if (!irrigador && umidade < valorLimiarBaixo)
+    irrigador = true;
+
+  if (irrigador && umidade > valorLimiarAlto)
+    irrigador = false;
+
+  document.getElementById("irrigador").style.display = irrigador ? "block" : "none";
+}
+
+function atualizarMedidores() {
+  document.getElementById("valorUmidadeTexto").innerHTML = `${umidade} %`
+  document.getElementById("valorUmidade").style.height = `${umidade}%`
+}
+
+function inicio() {
+  document.getElementById("LB_25").checked = true;
+  document.getElementById("LA_60").checked = true;
+}
+
+setTimeout(() => {
+  inicio();
+}, 100);
+
+setInterval(() => {
+  rotinaJardim();
+  atualizarMedidores();
+  rotinaIrrigador();
+}, 500)
