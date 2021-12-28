58d474e2599a71d0e1e472a0748791d49476e6b6
Gustavo Cesário <gs.cesario1998@gmail.com>
2021-12-28 14:48:56 -0300

    v2

--- conforto/index.js
+++ conforto/index.js
@@ -6,30 +6,42 @@ let som = 0;
 
 let conforto = 50;
 
+let arCondicionadoAutomatico = false;
+let umidificadorAutomatico = false;
+
+let statusArCondicionado = false;
+let statusUmidificador = false;
+
 function calcularConforto() {
   const deltaTemperatura = Math.abs(temperatura - 25);
   const deltaUmidade = Math.abs(umidade - 60);
   const deltaSom = som;
 
-  const confortoTemperatura = (1 - (deltaTemperatura / 25)) * 100;
-  const confortoUmidade = (1 - (deltaUmidade / 60)) * 100;
-  const confortoSom = (1 - (deltaSom / 120)) * 100;
-  
+  const confortoTemperatura = (1 - deltaTemperatura / 25) * 100;
+  const confortoUmidade = (1 - deltaUmidade / 60) * 100;
+  const confortoSom = (1 - deltaSom / 120) * 100;
+
   conforto = Math.min(confortoTemperatura, confortoUmidade, confortoSom);
 }
 
 function atualizarMedidorConforto() {
   calcularConforto();
   handleSmile(conforto);
-  document.getElementById("spanTemperatura").innerHTML = `Temperatura: ${Math.ceil(temperatura)} °C`
-  document.getElementById("spanUmidade").innerHTML = `Umidade: ${Math.ceil(umidade)}%`
-  document.getElementById("spanSom").innerHTML = `Som: ${Math.ceil(som)} dB`
-  document.getElementById("spanConforto").innerHTML = `Conforto: ${Math.ceil(conforto)}%`
+  document.getElementById(
+    "spanTemperatura"
+  ).innerHTML = `Temperatura: ${Math.ceil(temperatura)} °C`;
+  document.getElementById("spanUmidade").innerHTML = `Umidade: ${Math.ceil(
+    umidade
+  )}%`;
+  document.getElementById("spanSom").innerHTML = `Som: ${Math.ceil(som)} dB`;
+  document.getElementById("spanConforto").innerHTML = `Conforto: ${Math.ceil(
+    conforto
+  )}%`;
 }
 
 function getCorSorriso(value) {
   const hue = ((1 - value) * 120).toString(10);
-  return ["hsl(", hue, ",60%,60%)"].join("");
+  return `hsl(${hue},60%,60%)`;
 }
 
 function clickAutomatico(element) {
@@ -53,28 +65,37 @@ function clicarMedidor(e, tipo) {
 }
 
 function gerarValoresAmbiente() {
-  if (!automatico)
-    return;
-  const deltaTemperatura = (Math.random() * 4) - 2;
+  if (!automatico) return;
+  const deltaTemperatura = Math.random() * 4 - (statusArCondicionado ? 3 : 2);
   const novaTemperatura = temperatura + deltaTemperatura;
   temperatura = Math.min(50, Math.max(0, novaTemperatura));
 
-  const deltaUmidade = (Math.random() * 4) - 2;
+  const deltaUmidade = Math.random() * 4 - (statusUmidificador ? 1 : 2) - (statusArCondicionado ? 1 : 0);
   const novaUmidade = umidade + deltaUmidade;
   umidade = Math.min(100, Math.max(0, novaUmidade));
 
-  const deltaSom = (Math.random() * 40) - 30;
+  const deltaSom = Math.random() * 40 - 30;
   const novaSom = som + deltaSom;
   som = Math.min(100, Math.max(0, novaSom));
 }
 
 function atualizarMedidores() {
-  document.getElementById("valorTemperatura").style.height = `${Math.ceil((temperatura / 50) * 100)}%`
-  document.getElementById("valorTemperaturaTexto").innerHTML = `${Math.ceil(temperatura)} °C`
-  document.getElementById("valorUmidade").style.height = `${Math.ceil((umidade / 100) * 100)}%`
-  document.getElementById("valorUmidadeTexto").innerHTML = `${Math.ceil(umidade)} %`
-  document.getElementById("valorSom").style.height = `${Math.ceil((som / 120) * 100)}%`
-  document.getElementById("valorSomTexto").innerHTML = `${Math.ceil(som)} dB`
+  document.getElementById("valorTemperatura").style.height = `${Math.ceil(
+    (temperatura / 50) * 100
+  )}%`;
+  document.getElementById("valorTemperaturaTexto").innerHTML = `${Math.ceil(
+    temperatura
+  )} °C`;
+  document.getElementById("valorUmidade").style.height = `${Math.ceil(
+    (umidade / 100) * 100
+  )}%`;
+  document.getElementById("valorUmidadeTexto").innerHTML = `${Math.ceil(
+    umidade
+  )} %`;
+  document.getElementById("valorSom").style.height = `${Math.ceil(
+    (som / 120) * 100
+  )}%`;
+  document.getElementById("valorSomTexto").innerHTML = `${Math.ceil(som)} dB`;
 
   atualizarMedidorConforto();
 }
@@ -82,20 +103,61 @@ function atualizarMedidores() {
 function handleSmile(conforto) {
   const val = conforto;
   const corSmile = getCorSorriso(1 - conforto / 100);
-  document.getElementById("sorriso").style.borderRadius = Math.abs(50 - val) + "%";
-  if (val < 50) document.getElementById("sorriso").style.transform = 'rotate(' + 180 + 'deg) translateY(-50px)';
-  else document.getElementById("sorriso").style.transform = 'rotate(' + 0 + 'deg)';
+  document.getElementById("sorriso").style.borderRadius =
+    Math.abs(50 - val) + "%";
+  if (val < 50)
+    document.getElementById("sorriso").style.transform =
+      "rotate(" + 180 + "deg) translateY(-50px)";
+  else
+    document.getElementById("sorriso").style.transform = "rotate(" + 0 + "deg)";
 
   document.getElementById("smiley").style.backgroundColor = corSmile;
 }
 
+function rotinaControladores() {
+  if (arCondicionadoAutomatico) {
+    if (temperatura > 30 && !statusArCondicionado) statusArCondicionado = true;
+    else if (temperatura <= 25 && statusArCondicionado)
+      statusArCondicionado = false;
+  } else statusArCondicionado = false;
+
+  if (umidificadorAutomatico) {
+    if (umidade < 40 && !statusUmidificador) statusUmidificador = true;
+    else if (umidade >= 60 && statusUmidificador) statusUmidificador = false;
+  } else statusUmidificador = false;
+
+  const elementoArCondicionado = document.getElementById(
+    "statusArCondicionado"
+  );
+  const elementoUmidificador = document.getElementById("statusUmidificador");
+
+  elementoArCondicionado.innerHTML = statusArCondicionado
+    ? "Ativado"
+    : "Desativado";
+  elementoUmidificador.innerHTML = statusUmidificador
+    ? "Ativado"
+    : "Desativado";
+
+  elementoArCondicionado.style.color = statusArCondicionado ? "green" : "red";
+  elementoUmidificador.style.color = statusUmidificador ? "green" : "red";
+}
+
+function handleInputControlador(e, tipo) {
+  if (tipo === "arCondicionado") arCondicionadoAutomatico = e.checked;
+  if (tipo === "umidificador") umidificadorAutomatico = e.checked;
+}
+
 setTimeout(() => {
-  document.getElementById("medidorTemperatura").onclick = (e) => clicarMedidor(e, "temperatura")
-  document.getElementById("medidorUmidade").onclick = (e) => clicarMedidor(e, "umidade")
-  document.getElementById("medidorSom").onclick = (e) => clicarMedidor(e, "som")
+  document.getElementById("medidorTemperatura").onclick = (e) =>
+    clicarMedidor(e, "temperatura");
+  document.getElementById("medidorUmidade").onclick = (e) =>
+    clicarMedidor(e, "umidade");
+  document.getElementById("medidorSom").onclick = (e) =>
+    clicarMedidor(e, "som");
 }, 1000);
 
 setInterval(() => {
   gerarValoresAmbiente();
   atualizarMedidores();
-}, 1000);
+  rotinaControladores();
+}, 100);
--- irrigador/index.js
+++ irrigador/index.js
@@ -1,37 +1,54 @@
 let umidade = 50;
 let irrigador = false;
 
+const limiares = {
+  umidadeAlta: {
+    alto: 90,
+    baixo: 40,
+  },
+  umidadeMedia: {
+    alto: 75,
+    baixo: 25,
+  },
+  umidadeBaixa: {
+    alto: 60,
+    baixo: 10,
+  },
+};
+
 function rotinaJardim() {
   let velocidadeBase = -1;
 
-  if (irrigador)
-    velocidadeBase = 3;
+  if (irrigador) velocidadeBase = 3;
 
   let novaUmidade = umidade + velocidadeBase;
-  umidade = Math.max(0, Math.min(100, novaUmidade))
+  umidade = Math.max(0, Math.min(100, novaUmidade));
 }
 
 function rotinaIrrigador() {
-  const limiarAlto = document.querySelector('input[name="LA"]:checked').value;
-  const limiarBaixo = document.querySelector('input[name="LB"]:checked').value;
+  const seletor = document.getElementById("seletorUmidade");
+  const umidadeSelecionada = seletor.options[seletor.selectedIndex].value;
+  const limiarAlto = limiares[umidadeSelecionada].alto;
+  const limiarBaixo = limiares[umidadeSelecionada].baixo;
 
-  const valorLimiarAlto = Number(limiarAlto.replace("%", ""))
-  const valorLimiarBaixo = Number(limiarBaixo.replace("%", ""))
+  if (!irrigador && umidade < limiarBaixo) irrigador = true;
 
-  if (!irrigador && umidade < valorLimiarBaixo)
-    irrigador = true;
+  if (irrigador && umidade > limiarAlto) irrigador = false;
 
-  if (irrigador && umidade > valorLimiarAlto)
-    irrigador = false;
-
-  document.getElementById("irrigador").style.display = irrigador ? "block" : "none";
-  document.getElementById("mensagemIrrigador").innerHTML = `Irrigador ${irrigador ? "Ligado" : "Desligado"}`
-  document.getElementById("mensagemIrrigador").style.color = irrigador ? "#66BB6A" : "#EF5350"
+  document.getElementById("irrigador").style.display = irrigador
+    ? "block"
+    : "none";
+  document.getElementById("mensagemIrrigador").innerHTML = `Irrigador ${
+    irrigador ? "Ligado" : "Desligado"
+  }`;
+  document.getElementById("mensagemIrrigador").style.color = irrigador
+    ? "#66BB6A"
+    : "#EF5350";
 }
 
 function atualizarMedidores() {
-  document.getElementById("valorUmidadeTexto").innerHTML = `${umidade} %`
-  document.getElementById("valorUmidade").style.height = `${umidade}%`
+  document.getElementById("valorUmidadeTexto").innerHTML = `${umidade} %`;
+  document.getElementById("valorUmidade").style.height = `${umidade}%`;
 }
 
 function inicio() {
@@ -47,4 +64,4 @@ setInterval(() => {
   rotinaJardim();
   atualizarMedidores();
   rotinaIrrigador();
-}, 500)
+}, 250);
